0d2afd51022c2fc3e88f15cc364df7d78103d341
Soumadeep Sarkar <soumadeepsarkar1@gmail.com>
2021-06-21 22:45:32 +0530

    Added result history and saved in database

--- predictor/models.py
+++ predictor/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class TestResult(models.Model):
+    name=models.CharField(default="name",max_length=100)
     age=models.DecimalField(max_digits=6,decimal_places=2)
     sex=models.SmallIntegerField()
+    date_time=models.DateTimeField(null=True)
     cp=models.DecimalField(max_digits=6,decimal_places=2)
     trestbps=models.DecimalField(max_digits=6,decimal_places=2)
     chol=models.DecimalField(max_digits=6,decimal_places=2)
@@ -15,3 +17,5 @@ class TestResult(models.Model):
     slope=models.DecimalField(max_digits=6,decimal_places=2)
     ca=models.DecimalField(max_digits=6,decimal_places=2)
     thal=models.DecimalField(max_digits=6,decimal_places=2)
+    modelChoice=models.CharField(default="model",max_length=30)
+    result=models.CharField(max_length=30)
--- predictor/views.py
+++ predictor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import DataInputForm
+from django.utils import timezone
+from .models import TestResult
 import pickle
 def getPrediction(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal,modelChoice):
     dtree = pickle.load(open("decisionTree.sav", "rb"))
@@ -21,6 +23,26 @@ def getPrediction(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slo
     }
     prediction=modelDict[modelChoice].predict(sc.transform([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]]))
     return(prediction)
+def saveResult(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,
+        ca,thal,name,modelChoice,result):
+    timezone.activate('Asia/Kolkata')
+    date_time=timezone.localtime()
+    print(date_time)
+    model_names= {
+        "gaussianNB":"Gaussian Naive Bayes",
+        "knn":"K-nearest Neighbors",
+        "logReg":"Logistic Regression",
+        "dtree":"Decision Tree",
+        "rfc":"Random Forest Classifier",
+        "sdg":"Stochastic Gradient Descent",
+        "SVC":"Support Vector Classifier"
+    }
+    TestResult.objects.create(age=age, sex=sex, cp=cp,trestbps=trestbps,
+        chol=chol, fbs=fbs, restecg=restecg, thalach=thalach, exang=exang,
+        oldpeak=oldpeak, slope=slope, ca=ca, thal=thal,
+        date_time=date_time,name=name,modelChoice=model_names[modelChoice],
+        result=result
+    )
 # Create your views here.
 def data_input_view(request):
     form = DataInputForm()
@@ -56,6 +78,8 @@ def result_view(request):
             "1":"Heart disease present"
         }
         context = {'result': result[str(prediction[0])]}
+        saveResult(age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,
+        slope,ca,thal,name,modelChoice,result[str(prediction[0])])
     return(render(request,"result.html",context))
 def result_history_view(request):
-    return(render(request,"resultHistory.html",{}))
+    return(render(request,"resultHistory.html",{"querySet":TestResult.objects.all()}))
